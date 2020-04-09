19b1764ed462c00c003fb08d4ed3b712ddd04bd1
Stefan de Haas Hansen <stef9239@edu.easj.dk>
2020-04-09 14:47:36 +0200

    Persistency doesnt work, need generic(?)

---
+++ MVVM/Persistency/PersistencyService.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM.Persistency
+{
+    class PersistencyService <T>
+    {
+        public const string ServerUrl = "http://localhost:50466/";
+
+
+        public static List<T> GetEvents()
+        {
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.UseDefaultCredentials = true;
+
+            List<T> listHotels = new List<T>();
+            using (var client = new HttpClient(clientHandler))
+            {
+                client.BaseAddress = new Uri(ServerUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                try
+                {
+                    var response = client.GetAsync("api/object").Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var Tobject = response.Content.ReadAsAsync<IEnumerable<T>>().Result;
+                        foreach (var ho in Tobject)
+                        {
+                            listHotels.Add(ho);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+            return listHotels;
+
+        }
+
+        public static void AddTobject(T ho)
+        {
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.UseDefaultCredentials = true;
+            using (var client = new HttpClient(clientHandler))
+            {
+                client.BaseAddress = new Uri(ServerUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                try
+                {
+                    var response = client.PostAsJsonAsync("api/object", ho).Result;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+        }
+
+        public static void DeleteTobject(T ho)
+        {
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.UseDefaultCredentials = true;
+            using (var client = new HttpClient(clientHandler))
+            {
+                client.BaseAddress = new Uri(ServerUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                try
+                {
+                    var response = client.DeleteAsync($"api/object/{ho.Id}").Result;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+        }
+
+
+    }
+}
+
